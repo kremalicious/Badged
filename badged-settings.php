9ce0161df3ad639e46f12a3d3bb6176d29b6bdb9
kremalicious <kremalicious@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-17 21:11:20 +0000

    Renaming the plugin to Badged
    git-svn-id: http://plugins.svn.wordpress.org/badged/trunk@476939 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- badged-settings.php
+++ badged-settings.php
@@ -1,32 +1,32 @@
-<div class="wrap" id="bubblesoptions">
+<div class="wrap" id="badgedoptions">
 		
 		<header>
 			<div class="icon32"></div>
-			<h2><?php _e( 'Bubbles Settings', 'bbls' ); ?></h2>
+			<h2><?php _e( 'Badged Settings', 'bdgd' ); ?></h2>
 		</header>
 		
 		<form method="post" action="options.php">
-			<?php settings_fields( 'bubbles' ); ?>
+			<?php settings_fields( 'badged' ); ?>
 
 			<table class="form-table">
 				
 				<tr valign="top">
 					<td colspan="2">
-						<h2><?php _e( 'View', 'bbls' ); ?></h2>
+						<h2><?php _e( 'View', 'bdgd' ); ?></h2>
 					</td>
 				</tr>
 				
 				<tr valign="top">
 					<th scope="row" class="indent">
-						<?php _e( 'Style Notifications in', 'bbls' ); ?>
+						<?php _e( 'Style Notifications in', 'bdgd' ); ?>
 					</th>
 					<td>
 						<fieldset>
 							<input type="checkbox" name="menu" id="menu" value="yes"<?php echo get_option('menu') == 'yes' ? ' checked' : '';?> />
-							<label for="menu"><?php _e( 'Admin Menu', 'bbls' ); ?></label>
+							<label for="menu"><?php _e( 'Admin Menu', 'bdgd' ); ?></label>
 							<br />
 							<input type="checkbox" name="bar" id="bar" value="yes"<?php echo get_option('bar') == 'yes' ? ' checked' : '';?> />
-							<label for="bar"><?php _e( 'Toolbar', 'bbls' ); ?></label>
+							<label for="bar"><?php _e( 'Toolbar', 'bdgd' ); ?></label>
 						</fieldset>
 					</td>
 				</tr>	
@@ -38,7 +38,7 @@
 		</form>
 		
 		<footer>
-			<p><?php _e('Thanks for using Bubbles. Created by', 'bbls'); ?> <a href="http://mkretschmann.com">Matthias Kretschmann</a> (<a href="https://twitter.com/kremalicious">@kremalicious</a>)</p>
+			<p><?php _e('Thanks for using Badged. Created by', 'bdgd'); ?> <a href="http://mkretschmann.com">Matthias Kretschmann</a> (<a href="https://twitter.com/kremalicious">@kremalicious</a>)</p>
 		</footer>
 		
 	</div>
