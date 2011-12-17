e35c73a0885f2daff63ccc7c22c6e645867d24fe
kremalicious <kremalicious@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-17 21:08:03 +0000

    New options page to control notification styles in adminbar and/or admin menu - split up the css for this, add subtle options page css to the admin menu css - values are both set by default upon activation - new quick 32px icon
    git-svn-id: http://plugins.svn.wordpress.org/badged/trunk@476933 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bubbles.php
+++ bubbles.php
@@ -9,22 +9,50 @@
  * License: 		GPL
  */
 
-
-/**
- * Throw in the styles
- *
- * Enqueue the css file
- *
- * @since 0.1.0
- */
+ 
+if (function_exists('load_plugin_textdomain')) {
+	load_plugin_textdomain('bbls', false, dirname(plugin_basename(__FILE__)).'/languages' );
+}
  
 function bubbles_init() {
-	wp_register_style('bubbles-css', plugins_url('/bubbles/bubbles.css'), false, '9001');
-	wp_enqueue_style('bubbles-css');
+	
+	bubbles_register_settings();
+	if ( get_option('menu') == 'yes') {
+		wp_register_style('bubbles-menu-css', plugins_url('/bubbles/bubbles-menu.css'), false, '9001');
+		wp_enqueue_style('bubbles-menu-css');
+	}
+	
+	if ( get_option('bar') == 'yes') {
+		wp_register_style('bubbles-bar-css', plugins_url('/bubbles/bubbles-bar.css'), false, '9001');
+		wp_enqueue_style('bubbles-bar-css');
+	}
+	
+}
+
+function bubbles_settings() {
+	add_options_page('Bubbles Options', 'Bubbles', 'manage_options', 'bubbles_settings', 'bubbles_settings_page');
+}
+
+function bubbles_register_settings() {
+	register_setting('bubbles', 'menu');
+	register_setting('bubbles', 'bar');
+}
+
+function bubbles_settings_page() { 
+	require_once('bubbles-settings.php'); 
+}
+
+function bubbles_activation() {
+	bubbles_register_settings();
+	update_option('menu', 'yes');
+	update_option('bar', 'yes');
 }
 
 if ( is_admin() ) {
 	add_action('admin_init', 'bubbles_init');
+	add_action('admin_menu', 'bubbles_settings');
 }
+
+register_activation_hook(__FILE__, 'bubbles_activation');
 
 ?>
