51ac00b1d0fb70941a4c1f3af29165ae3014d16e
Matthias Kretschmann <matthias.kretschmann@ezeep.com>
2013-08-07 21:55:35 +0200

    refactor all the things

--- badged.php
+++ badged.php
@@ -1,14 +1,33 @@
 <?php
 /**
+ * Badged
+ *
+ * Transforms the standard WordPress update & comment notification badges into iOS-styled ones.
+ * Just activate and enjoy the red badges.
+ *
+ * @package   Badged
+ * @author    Matthias Kretschmann <desk@kremalicious.com>
+ * @license   GPL-2.0+
+ * @link      http://kremalicious.com/badged/
+ * @copyright 2013 Matthias Kretschmann
+ *
+ * @wordpress-plugin
  * Plugin Name: 	Badged
  * Plugin URI: 		http://kremalicious.com/badged/
  * Description: 	Transforms the standard WordPress update & comment notification badges into iOS-styled ones. Just activate and enjoy the red badges.
  * Author: 			Matthias Kretschmann
  * Author URI: 		http://matthiaskretschmann.com
- * Version: 		0.3.6
- * License: 		GPL
+ * Version: 		1.0.0
+ * License:     	GPL-2.0+
+ * License URI: 	http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: 	/lang
+ * Text Domain: 	bdgd
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 /**
  * Make the plugin work if symlinked
@@ -33,106 +52,18 @@ else if (isset($network_plugin)) {
 }
 
 define('BADGED_PLUGIN_FILE', $badged_plugin_file);
-define('BADGED_PLUGIN_PATH', WP_PLUGIN_DIR.'/'.basename(dirname($badged_plugin_file)));
-
-
-/**
- * Load translation
- *
- */
-if (function_exists('load_plugin_textdomain')) {
-	load_plugin_textdomain('bdgd', false, BADGED_PLUGIN_PATH.'/languages' );
-}
-
+//define('BADGED_PLUGIN_PATH', WP_PLUGIN_DIR.'/'.basename(dirname($badged_plugin_file)));
 
 /**
- * Plugin version, used for cache-busting of style and script file references.
+ * Let's roll
  *
- * @since   1.0.0
- *
- * @var     string
- */
-$version = '1.0.0';
-
-
-/**
- * Register the styles depending on options
+ * @since 1.0.0
  *
  */
-function badged_init() {
-	badged_register_settings();
-	wp_enqueue_style( 'badged-admin-styles', plugins_url( 'css/options.css', BADGED_PLUGIN_FILE), false, $version );
-	
-	if ( get_option('menu') == 'yes') {
-		wp_register_style('badged-menu-css', plugins_url('css/badged-menu.css', BADGED_PLUGIN_FILE), false, $version);
-		wp_enqueue_style('badged-menu-css');
-	}
-	
-	if ( get_option('bar') == 'yes') {
-		wp_register_style('badged-bar-css', plugins_url('css/badged-bar.css', BADGED_PLUGIN_FILE), false, $version);
-		wp_enqueue_style('badged-bar-css');
-	}
-}
 
-function badged_bar_only_init() {
-	if ( get_option('bar') == 'yes') {
-		wp_register_style('badged-bar-css', plugins_url('css/badged-bar.css', BADGED_PLUGIN_FILE), false, $version);
-		wp_enqueue_style('badged-bar-css');
-	}
-}
+require_once( plugin_dir_path( BADGED_PLUGIN_FILE ) . 'class-badged.php' );
 
-/**
- * Create the options page with our settings
- *
- */
-function badged_settings() {
-	add_options_page('Badged Options', 'Badged', 'manage_options', 'badged_settings', 'badged_settings_page');
-}
+register_activation_hook( BADGED_PLUGIN_FILE, array( 'Badged', 'activate' ) );
+register_deactivation_hook( BADGED_PLUGIN_FILE, array( 'Badged', 'deactivate' ) );
 
-function badged_register_settings() {
-	register_setting('badged', 'menu');
-	register_setting('badged', 'bar');
-	register_setting('badged', 'ios6');
-	register_setting('badged', 'ios7');
-}
-
-function badged_settings_page() { 
-	require_once('badged-settings.php'); 
-}
-
-
-/**
- * Set default options upon activation
- *
- */
-function badged_activation() {
-	badged_register_settings();
-	update_option('menu', 'yes');
-	update_option('bar', 'yes');
-	update_option('ios6', 'yes');
-}
-
-if ( is_admin() ) {
-	add_action('admin_init', 'badged_init');
-	add_action('admin_menu', 'badged_settings');
-} elseif ( !is_admin() && get_option('bar') == 'yes' ) {
-	add_action('admin_bar_init', 'badged_bar_only_init');
-}
-
-register_activation_hook(BADGED_PLUGIN_FILE, 'badged_activation');
-
-
-/**
- * Add settings link on plugin page
- *
- */
-function badged_settings_link($links) { 
-  $settings_link = '<a href="options-general.php?page=badged_settings">'. __('Settings') .'</a>'; 
-  array_unshift($links, $settings_link); 
-  return $links; 
-}
- 
-$plugin = plugin_basename(BADGED_PLUGIN_FILE); 
-add_filter('plugin_action_links_'.$plugin, 'badged_settings_link' );
-
-?>
+Badged::get_instance();
