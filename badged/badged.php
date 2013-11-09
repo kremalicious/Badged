69abb14e47a21379f5b40b3d714ca5acbdac3df9
Matthias Kretschmann <desk@kremalicious.com>
2013-11-09 23:39:05 +0100

    add github url for github updater plugin

--- badged/badged.php
+++ badged/badged.php
@@ -22,6 +22,7 @@
  * License URI: 	http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path: 	/languages
  * Text Domain: 	bdgd
+ * GitHub Plugin URI: https://github.com/kremalicious/Badged
  */
 
 // If this file is called directly, abort.
